d30e9b5c325accab4a2f960a8cd50bab3e0ba2ed
rohithpala <rohithpala02@gmail.com>
2022-02-27 21:24:31 +0530

    Two Sum Problem

---
+++ two-sum.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/two-sum/
+
+class Solution {
+   public int[] twoSum(int[] nums, int target) {
+      int i, j, n = nums.length;
+      for (i = 0; i < n; i++) {
+         for (j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] == target) {
+               return new int[] { i, j };
+            }
+         }
+      }
+      return null;
+   }
+}
