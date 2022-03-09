19ecd3d62800ddfb80aff9620b483e120b3fb952
rohithpala <rohithpala02@gmail.com>
2022-03-09 21:17:45 +0530

    Merging 2 Sorted Lists into one sorted list

--- MergeTwoSortedLists.java
+++ MergeTwoSortedLists.java
@@ -1,22 +1,5 @@
 // https://leetcode.com/problems/merge-two-sorted-lists/
 
-class ListNode {
-   int val;
-   ListNode next;
-
-   ListNode() {
-   }
-
-   ListNode(int val) {
-      this.val = val;
-   }
-
-   ListNode(int val, ListNode next) {
-      this.val = val;
-      this.next = next;
-   }
-}
-
 public class MergeTwoSortedLists {
    static ListNode newnode(int val) {
       return new ListNode(val, null);
